dc5595b8225547ffaced66fbc8fb6b8202db87ce
TitusChoi <chleleh812@naver.com>
2021-03-28 23:03:10 +0900

    [2021.03.28]

--- Solution/[B1260]DFS와 BFS.py
+++ Solution/[B1260]DFS와 BFS.py
@@ -1,15 +1,3 @@
 # Title         : [B1260]DFS와 BFS.py
 # Description   : dfs/bfs
 
-n, m, v=map(int,input().split()) # 첫 줄
-
-# 이중 배열 느낌으로 미리 할당
-matrix=[[0] * (n + 1) for i in range(n + 1)]
-
-for i in range(m):
-    x, y = map(int,input().split())
-    matrix[x][y] = matrix[x][y] = 1
-
-visit_list = [0]*(n + 1)
-
-print(matrix)
